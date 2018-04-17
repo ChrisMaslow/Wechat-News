5ab942244ea4b1a522e88645a4592a6b88a04c11
ChrisMa <405120508@qq.com>
2018-04-17 14:24:00 +0800

    Optimiz index.wxss

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,21 @@
-Page({})
+Page({
+  data: {
+    newsList: []
+  },
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: 'gn'
+      },
+      success: res => {
+        console.log(res.data.result)
+        let result = res.data.result
+        this.setData({
+          newsList: result
+        })
+      }
+    })
+
+  }
+})
